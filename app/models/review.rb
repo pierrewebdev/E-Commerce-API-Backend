6fd522b12e8c76f4eab9fa85ef17046637617ab4
pierrewebdev <patrick.pierre000@gmail.com>
2020-11-09 14:00:12 -0500

    fixed issue with review functionality by adding new methods to my review serializer

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+
+  def customer_name
+    self.customer.name
+  end
+
+  def product_id
+    self.product.id
+  end
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :headline,:content,:rating
+  attributes :headline,:content,:rating, :customer_name, :product_id
 end
