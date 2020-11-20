b404c024189d5f015b20b622e60c60dc523dd025
pierrewebdev <patrick.pierre000@gmail.com>
2020-11-19 23:16:29 -0500

    implemented increase and decrease routes and controller methods for cart_product model

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -18,6 +18,14 @@ class CartProductsController < ApplicationController
         render json: cart_product_to_update
     end
 
+    def decrease_quantity
+        cart_product_to_update = cart_product_to_update = CartProduct.find_by(product:params[:productId],cart:params[:cartId])
+
+        cart_product_to_update.update(quantity:cart_product_to_update.quantity - 1) 
+
+        render json: cart_product_to_update
+    end
+
     def delete
         #need the product id and the cart id
         product_to_delete = CartProduct.find_by(cart_id:params[:cartId],product_id:params[:productId])
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   #routes for cart_products controller
   post "/cart_products", to: "cart_products#create"
   patch "/increment-quantity", to: "cart_products#increase_quantity"
+  patch "/decrement-quantity", to: "cart_products#decrease_quantity"
   delete "delete-from-cart", to: "cart_products#delete"
 
 
