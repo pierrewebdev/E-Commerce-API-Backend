027dfa382167ee4b606c9d8b5ee64cfb5d7a9ff8
pierrewebdev <patrick.pierre000@gmail.com>
2020-11-27 21:19:16 -0500

    changes the origin in to match with the url of my react front end hosted on github pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://pierrewebdev.github.io/health-and-fit-store'
+    origins 'https://pierrewebdev.github.io/health-and-fit-store/#/'
 
     resource '*',
       headers: :any,
