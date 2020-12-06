4169a944a01805a36a06bd432cb5af4869d4f27d
pierrewebdev <patrick.pierre000@gmail.com>
2020-12-06 17:38:13 -0500

    got rid of extra byebug in customers_controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,7 +10,6 @@ class CustomersController < ApplicationController
         email = permit_login[:email]
         password = permit_login[:password]
         customer = Customer.find_by(email:email)
-        byebug
         if customer && customer.authenticate(password)
             customer_token = encode_token({customer_id:customer.id})
             render json: {customer_info:CustomerSerializer.new(customer),token:customer_token}
