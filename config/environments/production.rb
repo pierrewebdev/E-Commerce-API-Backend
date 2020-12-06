8c519a9ee468aa1039a3b59aa34eb459166fb5c8
pierrewebdev <patrick.pierre000@gmail.com>
2020-12-06 17:01:04 -0500

    added configuration code for action mailer so that it can work when app is in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,23 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+   #Action Mailer configurations for gmail smtp
+   config.action_mailer.delivery_method = :sendmail
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.default_options = {from: ENV["gmail_email"]}
+ 
+  
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'localhost:3000',
+   user_name:            ENV["gmail_email"],
+   password:             ENV["gmail_password"],
+   authentication:       'plain',
+   enable_starttls_auto: true  }
+ 
 end
